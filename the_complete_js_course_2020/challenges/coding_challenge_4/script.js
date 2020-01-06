014295488ef8fd38d9d8e30a5b6bd4ed6c53601e
Garrian <garrianbrown@gmail.com>
2020-01-06 01:31:24 -0600

    working through section 2.

--- the_complete_js_course_2020/challenges/coding_challenge_4/script.js
+++ the_complete_js_course_2020/challenges/coding_challenge_4/script.js
@@ -1,7 +1,7 @@
 /*
 CODING CHALLENGE 4
 
-Let's remember the first coding challenge whre Mark and John compared their BMI's. Let's now implement the same functionality with objects and methods.
+Let's remember the first coding challenge where Mark and John compared their BMI's. Let's now implement the same functionality with objects and methods.
 
 	1) For each of them, create an object with properties for their full name, mass, and height.
 	2) Then, add a mehtod to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
@@ -10,5 +10,34 @@ Let's remember the first coding challenge whre Mark and John compared their BMI'
 	Remeber: BMI = mass / height^2 "(height * height)" mass in kg and height in meters.
 */ 
 
+let people = {
+	john: {
+		first_name: 'John',
+		last_name: 'Smith',
+		mass: 92,
+		height: 1.95,
+		calculate_BMI: function(){
+	 		return this.bmi = Math.round( ( this.mass / ( this.height * this.height ) ) * 100 ) / 100 ;	
+	 	}
+	},
+	mark: {
+		first_name: 'Mark',
+		last_name: 'Hopkins',
+		mass: 78,
+		height: 1.69,
+		calculate_BMI: function(){
+	 		return this.bmi = Math.round( ( this.mass / ( this.height * this.height ) ) * 100 ) / 100 ;	
+	 	}
+	}
+};
 
+people.john.bmi = people.john.calculate_BMI();
+people.mark.bmi = people.mark.calculate_BMI();
 
+if ( people.john.bmi > people.mark.bmi ) {
+	console.log(` ${ people.john.first_name } ${ people.john.last_name } has a BMI of ${ people.john.bmi } which is larger than ${ people.mark.first_name } ${ people.mark.last_name }'s.` );
+} else if ( people.john.bmi < people.mark.bmi ) {
+	console.log( `${ people.mark.first_name } ${ people.mark.last_name } has a BMI of ${ people.mark.bmi } which is larger than ${ people.john.first_name } ${ people.john.last_name }'s.` );
+} else {
+	console.log( "They have the same BMI." );
+}
