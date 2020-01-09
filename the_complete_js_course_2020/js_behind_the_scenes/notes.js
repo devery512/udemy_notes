93a6807711a86530a36f831cabca7409a4b13ca9
Garrian <garrianbrown@gmail.com>
2020-01-08 23:55:53 -0600

    section 3

---
+++ the_complete_js_course_2020/js_behind_the_scenes/notes.js
@@ -0,0 +1,35 @@
+/*
+SECTION 3: HOW JavaScript Works Behind the Scenes
+
+
+
+*/
+
+
+/*
+	Execution Context Object
+		Variable Object (VO)
+		Scope Chain
+		"This" variable
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
