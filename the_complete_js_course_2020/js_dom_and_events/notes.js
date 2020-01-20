aee009548451ee943b7920808103b101027e49e4
Garrian <garrianbrown@gmail.com>
2020-01-20 16:00:01 -0600

    section 4 game

---
+++ the_complete_js_course_2020/js_dom_and_events/notes.js
@@ -0,0 +1,21 @@
+/* SECTION 4: JAVASCRIPT IN THE BROWSWER: DOM MANIPULATION AND EVENTS */
+
+/*
+THE DOCUMENT OBJECT MODEL
+	DOM: Document Object Model;
+	Structured representation of an HTML document;
+	The DOM is used to connect webpages to scripts like JavaScript;
+	For each TML box, there is an object in the DOM that we can access and interact with.
+
+
+
+*/
+
+
+
+
+
+
+
+
+
